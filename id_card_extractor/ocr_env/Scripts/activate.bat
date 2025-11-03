d4c0f94d0f4a8d08346f93503e9efbf7bf685dfc
Rohitpr05 <rprtuttu@gmail.com>
2025-11-03 23:18:08 +0530

    env isseu resolved

--- id_card_extractor/ocr_env/Scripts/activate.bat
+++ id_card_extractor/ocr_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rprtu\OneDrive\Desktop\ocr llm\id_card_extractor\ocr_env
+set VIRTUAL_ENV=C:\Users\rprtu\OneDrive\Desktop\Novares_legals\ocr llm\id_card_extractor\ocr_env
 
 if not defined PROMPT set PROMPT=$P$G
 
